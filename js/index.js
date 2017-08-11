1656a18cb00c9b467740eeb5fef6bba3c192a18b
ydc <630559190@qq.com>
2017-08-11 16:39:27 +0800

    add register page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 $(function() { 
     $('#myCarousel').carousel({interval:5000});//每隔5秒自动轮播 
+    
+    $(".headerdiv ul").on("click",function(e){
+         
+        switch (e.target.id) {
+            case "to-home":
+                $(".container").children(".home")
+                    .removeClass("hide").siblings().addClass("hide");
+                break;
+            case "to-download":
+                $(".container").children(".download")
+                    .removeClass("hide").siblings().addClass("hide");
+                break;
+            case "to-register":
+                $(".container").children(".register")
+                    .removeClass("hide").siblings().addClass("hide");
+                break;
+            default:
+                break;
+        
+        }
+    });    
 });
+
