5517fd86320f0afe43c509e312f649d7f8ccf66f
ydc <630559190@qq.com>
2017-08-23 17:06:13 +0800

    modify navbar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-$(function() { 
+$(function() {
+   
+
+
     $('#myCarousel').carousel({interval:5000});//每隔5秒自动轮播 
-    
-    $(".headerdiv ul").on("click",function(e){
+    $('.mynav').on('click',function(e) {
+        var a = e.target;
+        console.log($(a).parent('li').addClass('active').siblings('li'));
+        $(a).parent('li').addClass('active').siblings('li').removeClass('active'); 
+    });
+    $('.mynav').on('click',function(e){
          
         switch (e.target.id) {
             case "to-home":
@@ -25,4 +32,12 @@ $(function() {
         }
     });    
 });
+
+function iframeResize() {
+    console.log("jjjdd");
+     //alert(this.document.body.scrollHeight); //弹出当前页面的高度  
+    var obj = parent.document.getElementById("childFrame");  //取得父页面IFrame对象  
+    //alert(obj.height); //弹出父页面中IFrame中设置的高度  
+    obj.height = this.document.body.scrollHeight;  //调整父页面中IFrame的高度为此页面的高度  
+}
 
